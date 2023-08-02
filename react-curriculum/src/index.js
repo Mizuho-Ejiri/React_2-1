8139a8595801a6194583f799f53a105b0bfc1ab7
m_ejiri <103266782+Mizuho-Ejiri@users.noreply.github.com>
2023-08-02 17:05:07 +0900

    React2-1[temp] 一時提出

---
+++ react-curriculum/src/index.js
@@ -0,0 +1,25 @@
+import ReactDOM from "react-dom";
+
+const App = () => {
+
+  const handleClick = () => {
+    alert("submit!")
+  }
+  const handleClickAgain = () => {
+    alert("edit!")
+  }
+  const handleClickRepete = () => {
+    alert("delete!")
+  }
+
+  return (
+    <div className="flex justify-center">
+      <button className="rounded-full space-x-4 bg-pink-500" onClick={handleClick}>submit</button>
+      <button className="rounded-full space-x-4 bg-green-500" onClick={handleClickAgain}>edit</button>
+      <button className="rounded-full space-x-4 bg-yellow-500" onClick={handleClickRepete}>delete</button>
+    </div>
+  )
+}
+
+ReactDOM.render(<App />, document.getElementById('root'));
+export default App;
